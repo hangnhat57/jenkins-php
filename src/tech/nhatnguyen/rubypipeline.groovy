0e21b1d7861b7201cfb79b15d03b0b73d3707596
Nhat Nguyen <nhat@Akita-2.local>
2018-06-28 14:40:28 +0700

    Update ruby lib for jenkins

---
+++ src/tech/nhatnguyen/rubypipeline.groovy
@@ -0,0 +1,38 @@
+#!/usr/bin/env groovy
+package tech.nhatnguyen;
+
+
+def getcode(String giturl) {
+    stage("Checkout") {
+        checkout([$class                           : 'GitSCM',
+                  branches                         : [[name: "${BRANCH_NAME}"]],
+                  doGenerateSubmoduleConfigurations: false,
+                  extensions                       : [],
+                  submoduleCfg                     : [],
+                  userRemoteConfigs                : [[credentialsId: "${CREDENTIAL_GITHUB}", url: giturl]]])
+
+    }
+
+}
+def pulldocker(String image){
+    stage("Pull Latest Images"){
+        sh "sudo docker pull ${image}:latest"
+    }   
+}
+def buildbydocker(){
+    stage("Build"){
+        sh 'sudo docker-compose up  -d --force-recreate'
+        sh 'sudo docker-compose exec -T web bash docker/install.sh'           
+    }
+}
+def startdockerrails(){
+    stage('Start Server') {
+        sh 'sudo docker-compose exec -d -T web bash docker/startserver.sh'
+        } 
+    }
+def errhandlerwithdocker(){
+    currentBuild.result = 'FAILURE'
+    sh 'sudo docker-compose down'
+}
+
+
