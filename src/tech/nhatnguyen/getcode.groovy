3237dbe7eedd0d0e88d6dd27d6d5b96b6f5c453a
Nhat Nguyen <nhat@Akita-2.local>
2018-06-28 14:20:21 +0700

    Update

--- src/tech/nhatnguyen/getcode.groovy
+++ src/tech/nhatnguyen/getcode.groovy
@@ -1,16 +1,18 @@
 #!/usr/bin/env groovy
 package tech.nhatnguyen;
-
-def getcode(String giturl){
-    stage("Checkout"){
-        checkout([$class: 'GitSCM',
-            branches: [[name: "${BRANCH_NAME}"]],
-            doGenerateSubmoduleConfigurations: false,
-            extensions: [],
-            submoduleCfg: [],
-            userRemoteConfigs: [[credentialsId: "${CREDENTIAL_GITHUB}", url: giturl]]])
+class GetCode {
+    def getcode(String giturl) {
+        stage("Checkout") {
+            checkout([$class                           : 'GitSCM',
+                      branches                         : [[name: "${BRANCH_NAME}"]],
+                      doGenerateSubmoduleConfigurations: false,
+                      extensions                       : [],
+                      submoduleCfg                     : [],
+                      userRemoteConfigs                : [[credentialsId: "${CREDENTIAL_GITHUB}", url: giturl]]])
 
         }
 
+    }
+
+    
 }
-return this
