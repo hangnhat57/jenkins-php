94ccbfdce3132ec2f45fadebcacdca4368e4c39c
Nhat Nguyen <nhat@Akita-2.local>
2018-06-28 11:25:39 +0700

    Update package name

--- src/tech/nhatnguyen/getcode.groovy
+++ src/tech/nhatnguyen/getcode.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 package tech.nhatnguyen;
 
-def getsource(String giturl){
+def execute(String giturl){
     stage("Checkout"){
         checkout([$class: 'GitSCM',
             branches: [[name: "${BRANCH_NAME}"]],
